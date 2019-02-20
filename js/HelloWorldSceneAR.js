ab9d39e40588916f4f15aadc9ecd5fd33358698d
Parthiban Sugumar <parthibansugumar66@gmail.com>
2019-02-20 18:03:14 +0530

    Set of machines added!!

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -10,25 +10,52 @@ import {
   ViroARPlaneSelector,
 } from 'react-viro';
 
-
 var Machine = [
   {
-      location:'./res/CNC/CNC.obj',
-      res:'./res/CNC/CNC.mtl',
+      location:require('./res/CNC/CNC.obj'),
+      res:require('./res/CNC/CNC.mtl'),
       points: [0,0,0],
       machineID: 'machine-1',
   },
   {
-    location:'./res/CNC/CNC.obj',
-    res:'./res/CNC/CNC.mtl',
+    location:require('./res/CNC/CNC.obj'),
+    res:require('./res/CNC/CNC.mtl'),
     points: [1,0,0],
     machineID: 'machine-2',
-  }
+  },
+  {
+    location:require('./res/CNC/CNC.obj'),
+    res:require('./res/CNC/CNC.mtl'),
+    points: [2,0,-1],
+    machineID: 'machine-3',
+  },
+  {
+  location:require('./res/CNC/CNC.obj'),
+  res:require('./res/CNC/CNC.mtl'),
+  points: [0,0,-1],
+  machineID: 'machine-4',
+  },
+  {
+  location:require('./res/CNC/CNC.obj'),
+  res:require('./res/CNC/CNC.mtl'),
+  points: [3,0,-2],
+  machineID: 'machine-5',
+  },
 ]
 
 export default class HelloWorldSceneAR extends Component {
+
+  totalLength=20;
+  totalWidth=20;
+  LayoutLength=2;
+  LayoutWidth=2;
   
   render() {
+    this.scale = Math.min(
+      Math.floor(this.LayoutLength / this.totalLength),
+      Math.floor(this.LayoutWidth / this.totalWidth)
+  );
+  
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
         <ViroAmbientLight color={"#aaaaaa"}/>
@@ -38,8 +65,9 @@ export default class HelloWorldSceneAR extends Component {
         {
           Machine.map((machine) =>
             <Viro3DObject
-              source={require(machine.location)}
-              resorces={[require(machine.res)]}
+              source={machine.location}
+              resorces={machine.res}
+              key={machine.machineID}
               position={machine.points}
               scale={[.0001, .0001, .0001]}
               highAccuracyEvents={true}
