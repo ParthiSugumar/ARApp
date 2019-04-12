74433044a409464f36b57a807d19633655a18268
Parthiban Sugumar <parthibansugumar66@gmail.com>
2019-04-12 13:22:05 +0530

    Pre Final changes are made!!

--- js/HelloWorldScene.js
+++ js/HelloWorldScene.js
@@ -14,40 +14,71 @@ import {
   ViroAmbientLight,
   ViroSpotLight,
   ViroARPlaneSelector,
+  ViroImage,
   ViroNode,
   ViroAnimations,
 } from 'react-viro';
 
 var Machine = [
   {
-      location:require('./res/CNC/CNC.obj'),
-      res:require('./res/CNC/CNC.mtl'),
-      points: [0,0,0],
-      machineID: 'm-1',
+    location:require('./res/CNC/CNC.obj'),
+    res:require('./res/CNC/CNC.mtl'),
+    points: [0,0,0],
+    machineID: 'm-1',
+    d1:'machineOn:true',
+    d2:'spindleOn:true',
+    d3:'jog:false',
+    d4:'auto:true',
+    d5:'mdi:false',
+    d6:'alarm:false',
   },
   {
     location:require('./res/CNC/CNC.obj'),
     res:require('./res/CNC/CNC.mtl'),
     points: [1,0,0],
     machineID: 'm-2',
+    d1:'machineOn:true',
+    d2:'spindleOn:false',
+    d3:'jog:false',
+    d4:'auto:true',
+    d5:'mdi:false',
+    d6:'alarm:false',
   },
   {
     location:require('./res/CNC/CNC.obj'),
     res:require('./res/CNC/CNC.mtl'),
     points: [2,0,-1],
     machineID: 'm-3',
+    d1:'machineOn:true',
+    d2:'spindleOn:true',
+    d3:'jog:true',
+    d4:'auto:true',
+    d5:'mdi:false',
+    d6:'alarm:false',
   },
   {
-  location:require('./res/CNC/CNC.obj'),
-  res:require('./res/CNC/CNC.mtl'),
-  points: [0,0,-1],
-  machineID: 'm-4',
+    location:require('./res/CNC/CNC.obj'),
+    res:require('./res/CNC/CNC.mtl'),
+    points: [0,0,-1],
+    machineID: 'm-4',
+    d1:'machineOn:true',
+    d2:'spindleOn:true',
+    d3:'jog:false',
+    d4:'auto:true',
+    d5:'mdi:false',
+    d6:'alarm:true',
   },
   {
-  location:require('./res/CNC/CNC.obj'),
-  res:require('./res/CNC/CNC.mtl'),
-  points: [3,0,-2],
-  machineID: 'm-5',
+    location:require('./res/CNC/CNC.obj'),
+    res:require('./res/CNC/CNC.mtl'),
+    points: [3,0,-2],
+    machineID: 'm-5',
+    d1:'machineOn:false',
+    d2:'spindleOn:false',
+    d3:'jog:false',
+    d4:'auto:false',
+    d5:'mdi:false',
+    d6:'alarm:false',
   },
 ]
 
@@ -81,31 +112,115 @@ export default class HelloWorldScene extends Component {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
         <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-        <ViroAmbientLight color={"#aaaaaa"} />
+        <ViroAmbientLight color={"#aaaaaa"}/>
         <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]}
           position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
         <ViroARPlaneSelector width={this.LayoutWidth} height={this.LayoutLength}>
+          <ViroImage 
+            width={this.LayoutWidth} 
+            height={this.LayoutLength}
+            source={require('./res/tan.jpg')}
+            scale={[.9, .9, .9].map(x => x * this.scale)}
+            position={[2, 0, -0.5].map(x => x * this.scale)}
+            animation={{name:'rotate',run:true}}  
+          />
         {
           Machine.map((machine) =>
+          machine.machineID==this.state.selectedId?
             <Viro3DObject
+              onClick={() => this.setState({ selectedId : machine.machineID })}
+              source={machine.location}
+              resorces={machine.res}
+              key={machine.machineID}
+              position={machine.points.map(x => x * this.scale)}
+              scale={[.0001, .0001, .0001]}
+              highAccuracyEvents={true}
+              type="OBJ"/>:
+            <Viro3DObject
+              onClick={() => this.setState({ selectedId : machine.machineID })}
               source={machine.location}
               resorces={machine.res}
               key={machine.machineID}
               position={machine.points.map(x => x * this.scale)}
               scale={[.0001, .0001, .0001].map(x => x * this.scale)}
               highAccuracyEvents={true}
-              type="OBJ"
-              onClick={() => this.setState({ selectedId : machine.machineID })}/>
+              type="OBJ"/>
           )
         }
         {
           Machine.map((machine)=>
           machine.machineID==this.state.selectedId?
-              <ViroText 
+            <ViroText
               text={machine.machineID}
               key={machine.machineID}
-              scale={[.5, .5, .5].map(x => x * this.scale)} 
-              position={machine.points.map(x => (x-0.1) * this.scale)}
+              scale={[.1, .1, .1]} 
+              position={[(machine.points[0])*this.scale,(machine.points[1]+0.8)*this.scale,machine.points[2]*this.scale]}
+              style={styles.helloWorldTextStyle}/>:null
+          )
+        }
+        {
+          Machine.map((machine)=>
+          machine.machineID==this.state.selectedId?
+            <ViroText
+              text={machine.d1}
+              key={machine.machineID}
+              scale={[.1, .1, .1]} 
+              position={[(machine.points[0]+1.75)*this.scale,(machine.points[1]+0.5)*this.scale,machine.points[2]*this.scale]}
+              style={styles.helloWorldTextStyle}/>:null
+          )
+        }
+        {
+          Machine.map((machine)=>
+          machine.machineID==this.state.selectedId?
+            <ViroText
+              text={machine.d2}
+              key={machine.machineID}
+              scale={[.1, .1, .1]} 
+              position={[(machine.points[0]+1.75)*this.scale,(machine.points[1]+0.4)*this.scale,machine.points[2]*this.scale]}
+              style={styles.helloWorldTextStyle}/>:null
+          )
+        }
+        {
+          Machine.map((machine)=>
+          machine.machineID==this.state.selectedId?
+            <ViroText
+              text={machine.d3}
+              key={machine.machineID}
+              scale={[.1, .1, .1]} 
+              position={[(machine.points[0]+1.75)*this.scale,(machine.points[1]+0.3)*this.scale,machine.points[2]*this.scale]}
+              style={styles.helloWorldTextStyle}/>:null
+          )
+        }
+        {
+          Machine.map((machine)=>
+          machine.machineID==this.state.selectedId?
+            <ViroText
+              text={machine.d4}
+              key={machine.machineID}
+              scale={[.1, .1, .1]} 
+              position={[(machine.points[0]+1.75)*this.scale,(machine.points[1]+0.2)*this.scale,machine.points[2]*this.scale]}
+              style={styles.helloWorldTextStyle}/>:null
+          )
+        }
+        {
+          Machine.map((machine)=>
+          machine.machineID==this.state.selectedId?
+            <ViroText
+              text={machine.d5}
+              key={machine.machineID}
+              scale={[.1, .1, .1]} 
+              position={[(machine.points[0]+1.75)*this.scale,(machine.points[1]+0.1)*this.scale,machine.points[2]*this.scale]}
+              style={styles.helloWorldTextStyle}/>:null
+          )
+        }
+        {
+          Machine.map((machine)=>
+          machine.machineID==this.state.selectedId?
+            <ViroText
+              text={machine.d6}
+              key={machine.machineID}
+              scale={[.1, .1, .1]} 
+              position={[(machine.points[0]+1.75)*this.scale,(machine.points[1])*this.scale,machine.points[2]*this.scale]}
               style={styles.helloWorldTextStyle}/>:null
           )
         }
@@ -129,10 +244,14 @@ var styles = StyleSheet.create({
   helloWorldTextStyle: {
     fontFamily: 'Arial',
     fontSize: 30,
-    color: '#ffffff',
+    color: '#800080',
     textAlignVertical: 'center',
     textAlign: 'center',
   },
+});
+
+ViroAnimations.registerAnimations({
+  rotate:{properties:{rotateX:"-=90"},duration:1000}
 });
 
 module.exports = HelloWorldScene;
